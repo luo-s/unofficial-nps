10d193e66e3ad5e96a59eb569c6c4f1544e26eb5
Shu <luo.s@outlook.com>
2023-04-24 18:05:29 -0800

    adjust zoom ratio

--- app.js
+++ app.js
@@ -207,7 +207,7 @@ function updateParkDetail(data) {
     });
 
     // add map
-    let map = L.map("map").setView([latitude, longitude], 13);
+    let map = L.map("map").setView([latitude, longitude], 8);
     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
       maxZoom: 19,
       attribution:
